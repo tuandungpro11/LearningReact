4e3b4dd6a9ae8f114cb7f1c9f4879bbb82542f24
HOANG PHAM HUY <HOANG.PH184266@sis.hust.edu.vn>
2022-07-12 22:15:29 +0700

    oke

--- ValueInvest/src/app.js
+++ ValueInvest/src/app.js
@@ -3,3 +3,16 @@
     controller.addFooter(component.footer)
     controller.addEvent()
    
+
+let a = 2000;
+let b = 54;
+let c = 42000;
+let gr = 1 + b/100;
+let rs = a;
+let count = 0;
+while (rs < c) {
+    a = a*gr;
+    rs += a;
+    count++;
+} 
+console.log(count, rs);
